f3005d92970a148f8ddb8ea582e2cbb55d8a206a
JORGE A SARRIA <55598783+r2e2sarria@users.noreply.github.com>
2026-06-04 13:35:12 -0400

    Actualización ajustes 2026

--- acerca.php
+++ acerca.php
@@ -9,18 +9,19 @@ include 'templates/head.php';
     <label for="subtit">ACERCA DE<br>http://agendandote.com</label>
     <br>
     <div class="w70p centrar ml60 fs09">
-    <p>Este es un sitio creado como parte de la materia de PROCESO DE SOFTWARE PERSONAL (2022) de la facultad de Ingenieria de software del <b>Politécnico Grancolombia Institución Universitaria</b>, bajo la tutoria de la profesora Natalia Martinez por los estudiantes; </p>
+    <p>Este es un sitio creado como parte de la materia de INTEGRACIÓN CONTINUA (2026) de la facultad de Ingenieria de software del <b>Politécnico Grancolombia Institución Universitaria</b>, bajo la tutoria de la profesora Natalia Martinez por los estudiantes; </p>
     <div class="tac fs08">
-    <br>NORBERTO DE JESUS CARDONA<br>
-JHONNATAN ALFREDO GOMEZ<br>
-JULIAN ANDRES MORALES<br>
-SAUL LEONARDO BUITRAGO<br>
-JORGE ARTURO SARRIA
+    <br>CRISTIAN CASTILLO RODRIGUEZ<br>
+STEVE SEBASTIAN FOX LYNTON<br>
+NICOLAS GALLEGO RESTREPO<br>
+YEISON MIGUEL NORIEGA AMAYA<br>
+JORGE ARTURO SARRIA COBO
+<br>
     </div>
     <img src="images/logopoli.png" alt="Logo de Politécnico Grancolombiano "  class="logo3">
     </div>
     <div class="tac fs08 mt20">
-        CopyRight(c)2022 por sus desarrolladores.
+        CopyRight(c)2026 por sus desarrolladores.
     </div>
     <div class="col col-around w70p centro" style="margin-top:20px">
     <div><a href="politicas.php">Politicas</a></div>
